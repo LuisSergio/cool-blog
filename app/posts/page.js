3799234c76e7da9c6e3b7bc41b788c97aab794e8
LuisSergio <luisaraujo292@yahoo.com>
2023-10-27 16:24:31 -0300

    Added posts.

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,4 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function AllPostsPage()
+{
+    const[posts,setPosts]         = useState([]);
+    const[isLoading,setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch("/posts.json")
+            .then((response) => response.json())
+            .then((data) => {
+                setPosts(data.posts);
+                setIsLoading(false);
+            });
+    },[]);
+
+    if (isLoading) {
+        return <p>Loading ...</p>;
+    }
+
+    return(
+        <>
+            <h1>All Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+        </>
+    );
+    
+}
+
+/*
 export default function AllPostsPage()
 {
     return <h1>All Posts</h1>;
 }
+*/
